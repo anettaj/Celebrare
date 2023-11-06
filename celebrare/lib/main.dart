d49df24dd5f0305d28e052c38b82d76bcd23cb37
anettaj <58222128+anettaj@users.noreply.github.com>
2023-11-06 23:24:48 +0530

    Create main.dart

---
+++ celebrare/lib/main.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:celebrare/Home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: true,
+    home: Splash(),
+  ));
+}
+
+class Splash extends StatefulWidget {
+  @override
+  _Splash createState() => _Splash();
+}
+
+class _Splash extends State<Splash> {
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 1), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context)
+          => Home(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: Image.asset(
+            'assets/logo.png',
+            scale: 0.7,
+          ),
+        ),
+      ),
+    );
+  }
+}
